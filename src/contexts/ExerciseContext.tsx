4612219bc39638e27a56a27c55fbdb160b912d62
Ray Orunsolu <rorunsolu@gmail.com>
2025-05-16 22:34:32 +0100

    allows for creatioon of default exercises by me only for now

---
+++ src/contexts/ExerciseContext.tsx
@@ -0,0 +1,97 @@
+import { db } from "@/auth/Firebase";
+import { ExerciseContext } from "@/hooks/useExercisesHook";
+import { addDoc, collection } from "firebase/firestore";
+// Name
+// Muscle group
+// Secondary muscle group
+// How to do it
+
+// I will use this to create global default exercises that all users can use in their workouts but it will only be allowed to be edited by me. users cannot edit these exercises
+
+type MuscleGroupData =
+	| "Chest"
+	| "Back"
+	| "Shoulders"
+	| "Biceps"
+	| "Triceps"
+	| "Forearms"
+	| "Neck"
+	| "Abdominals"
+	| "Obliques"
+	| "Lower Back"
+	| "Glutes"
+	| "Quadriceps"
+	| "Hamstrings"
+	| "Calves"
+	| "Hip Flexors"
+	| "Adductors"
+	| "Lats";
+
+import { useState, type ReactNode } from "react";
+
+export interface ExerciseData {
+	name: string;
+	muscleGroup: MuscleGroupData;
+	secondaryMuscleGroup: MuscleGroupData;
+	howTo: string;
+}
+
+export interface ExerciseContextType {
+	exercises: ExerciseData[];
+	createExercise: (
+		name: string,
+		muscleGroup: MuscleGroupData,
+		secondaryMuscleGroup: MuscleGroupData,
+		howTo: string
+	) => Promise<string>;
+	// The promise type can't be a void otherwise the function won't be able to access any of the needed data
+}
+
+export const ExerciseProvider = ({ children }: { children: ReactNode }) => {
+	const [exercises, setExercises] = useState<ExerciseData[]>([]);
+
+	const createExercise = async (
+		name: string,
+		muscleGroup: MuscleGroupData,
+		secondaryMuscleGroup: MuscleGroupData,
+		howTo: string
+	) => {
+		try {
+			const data = {
+				name,
+				muscleGroup: muscleGroup as MuscleGroupData,
+				secondaryMuscleGroup: secondaryMuscleGroup as MuscleGroupData,
+				howTo,
+			};
+
+			// refers to the new exercise that is being created
+			const exerciseDocRef = await addDoc(collection(db, "excercises"), data);
+
+			// get the prev/current list, add the new exercise to along with the id at the point of creation
+			setExercises((prev) => [
+				...prev, // open up the exisitng list to make space for the new exercise
+				{
+					// below is the new exercise in question
+					...data,
+					id: exerciseDocRef.id,
+				},
+			]);
+
+			console.log("Exercise created: ", exerciseDocRef.id);
+			return exerciseDocRef.id;
+		} catch (error) {
+			throw new Error("Unable to create exercise.");
+		}
+	};
+
+	return (
+		<ExerciseContext.Provider
+			value={{
+				exercises,
+				createExercise,
+			}}
+		>
+			{children}
+		</ExerciseContext.Provider>
+	);
+};
