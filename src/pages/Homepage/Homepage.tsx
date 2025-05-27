b54c9cf0fabe69ff3d547fa2ef8e08b3454dc00a
Ray Orunsolu <rorunsolu@gmail.com>
2025-05-27 22:58:21 +0100

    no renders actual data from  firebase

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,5 +1,9 @@
-import { ChevronDown, ChevronUp, Ellipsis } from "lucide-react";
-import { useState } from "react";
+import { UserAuth } from "@/auth/AuthContext";
+import { useWorkOutHook } from "@/hooks/useWorkoutHook";
+import { formatDistanceToNow } from "date-fns";
+import { EllipsisVertical } from "lucide-react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import {
 	Card,
 	Text,
@@ -9,212 +13,159 @@ import {
 	Button,
 	Stack,
 	Container,
-	Divider,
+	SimpleGrid,
 	Title,
 } from "@mantine/core";
 
-const mockData = [
-	{
-		id: 1,
-		user: {
-			name: "rorunsolu1",
-			avatar: "https://i.pravatar.cc/150?img=1",
-			timeAgo: "8 hours ago",
-		},
-		title: "Leg Day",
-		note: "GG",
-		duration: "1h 15min",
-		volume: "15,065 kg",
-		exercises: [
-			"6 sets Leg Extension (Machine)",
-			"3 sets Hack Squat (Machine)",
-			"3 sets Lying Leg Curl (Machine)",
-			"3 sets Seated Leg Curl (Machine)",
-			"3 sets Standing Calf Raise",
-			"3 sets Glute Kickback",
-		],
-	},
-	{
-		id: 2,
-		user: {
-			name: "rorunsolu1",
-			avatar: "https://i.pravatar.cc/150?img=1",
-			timeAgo: "Yesterday",
-		},
-		title: "Pull Day + Mini Push",
-		note: "GG",
-		duration: "1h 9min",
-		volume: "9,782.5 kg",
-		exercises: [
-			"6 sets Iso-Lateral Low Row",
-			"4 sets Lat Pulldown",
-			"3 sets Seated Cable Row",
-			"3 sets Dumbbell Curl",
-			"3 sets Incline Chest Press",
-		],
-	},
-	{
-		id: 3,
-		user: {
-			name: "rorunsolu1",
-			avatar: "https://i.pravatar.cc/150?img=2",
-			timeAgo: "2 days ago",
-		},
-		title: "Push Day",
-		note: "Solid session",
-		duration: "1h 5min",
-		volume: "11,450 kg",
-		exercises: [
-			"4 sets Bench Press",
-			"3 sets Incline Dumbbell Press",
-			"3 sets Cable Flys",
-			"3 sets Tricep Dips",
-			"3 sets Overhead Tricep Extension",
-		],
-	},
-	{
-		id: 4,
-		user: {
-			name: "rorunsolu1",
-			avatar: "https://i.pravatar.cc/150?img=3",
-			timeAgo: "3 days ago",
-		},
-		title: "Back + Biceps",
-		note: "Felt strong",
-		duration: "1h 20min",
-		volume: "13,620 kg",
-		exercises: [
-			"4 sets Deadlift",
-			"4 sets Pull-ups",
-			"3 sets Barbell Row",
-			"3 sets EZ Bar Curl",
-			"3 sets Hammer Curl",
-		],
-	},
-];
-
-const WorkoutCard = ({ workout }: { workout: (typeof mockData)[0] }) => {
-	const [showAll, setShowAll] = useState(false);
-	const shownExercises = showAll
-		? workout.exercises
-		: workout.exercises.slice(0, 3);
-
-	return (
-		<Card
-			withBorder
-			radius="md"
-			shadow="sm"
-			mb="md"
-			bg="dark.8"
-			onClick={() => {
-				//navigate to link based on workout.id
-			}}
-		>
-			<Group justify="apart">
-				<Group>
-					<Avatar
-						src={workout.user.avatar}
-						radius="xl"
-					/>
-					<div>
-						<Text
-							size="sm"
-							fw={500}
-						>
-							{workout.user.name}
-						</Text>
-						<Text
-							size="xs"
-							c="dimmed"
-						>
-							{workout.user.timeAgo}
-						</Text>
-					</div>
-				</Group>
-				<Menu
-					withinPortal
-					position="bottom-end"
-					shadow="sm"
-				>
-					<Menu.Target>
-						<Button
-							variant="subtle"
-							c="gray"
-						>
-							<Ellipsis size={20} />
-						</Button>
-					</Menu.Target>
-					<Menu.Dropdown>
-						<Menu.Item>Edit</Menu.Item>
-						<Menu.Item c="red">Delete</Menu.Item>
-					</Menu.Dropdown>
-				</Menu>
-			</Group>
-
-			<Stack
-				mt="md"
-				gap={4}
-			>
-				<Title order={4}>{workout.title}</Title>
-				<Text size="sm">{workout.note}</Text>
-				<Group gap="xl">
-					<Text
-						size="sm"
-						c="dimmed"
-					>
-						Time
-						<br />
-						<strong>{workout.duration}</strong>
-					</Text>
-					<Text
-						size="sm"
-						c="dimmed"
-					>
-						Volume
-						<br />
-						<strong>{workout.volume}</strong>
-					</Text>
-				</Group>
-				<Divider my="sm" />
-				{shownExercises.map((ex, idx) => (
-					<Text
-						key={idx}
-						size="sm"
-					>
-						{ex}
-					</Text>
-				))}
-				{workout.exercises.length > 3 && (
-					<Button
-						variant="subtle"
-						size="xs"
-						onClick={() => setShowAll((s) => !s)}
-						rightSection={
-							showAll ? <ChevronUp size={16} /> : <ChevronDown size={16} />
-						}
-					>
-						{showAll
-							? "Show less"
-							: `See ${workout.exercises.length - 3} more exercises`}
-					</Button>
-				)}
-			</Stack>
-		</Card>
-	);
-};
-
 const Home = () => {
+	const { workouts, fetchWorkouts, deleteWorkout } = useWorkOutHook();
+	const { user } = UserAuth();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			await fetchWorkouts();
+		};
+
+		fetchData();
+	}, []);
+
 	return (
 		<Container
-			size="xs"
-			py="xs"
+			size="md"
+			p="xs"
+			py="md"
 		>
-			{mockData.map((workout) => (
-				<WorkoutCard
-					key={workout.id}
-					workout={workout}
-				/>
-			))}
+			<Stack gap="xl">
+				<Stack gap="xs">
+					<Title order={1}>Your Workouts</Title>
+					<Text c="dimmed">
+						View your recent workouts below. Click on a workout to see details,
+						or use the menu to delete a workout.
+					</Text>
+				</Stack>
+
+				<SimpleGrid
+					spacing="md"
+					cols={{ base: 1, sm: 2, lg: 2 }}
+				>
+					{workouts.map((workout) => {
+						return (
+							<Card
+								key={workout.id}
+								withBorder
+								radius="md"
+								shadow="md"
+								bg="dark.9"
+								onClick={() => {
+									navigate(`/workout/${workout.id}`, {
+										state: { workout },
+									});
+								}}
+								className="cursor-pointer"
+							>
+								<Group justify="space-between">
+									<Group>
+										<Avatar
+											src={
+												user?.photoURL ??
+												"https://www.svgrepo.com/show/508699/landscape-placeholder.svg"
+											}
+											radius="xl"
+										/>
+										<div>
+											<Text
+												size="sm"
+												fw={500}
+											>
+												{user?.displayName}
+											</Text>
+											<Text
+												size="xs"
+												c="dimmed"
+											>
+												{workout.dateOfWorkout
+													? formatDistanceToNow(
+															workout.dateOfWorkout.toDate(),
+															{
+																addSuffix: true,
+															}
+														)
+													: "Unknown Date"}
+											</Text>
+										</div>
+									</Group>
+									<Menu
+										withinPortal
+										position="bottom-end"
+										shadow="md"
+									>
+										<Menu.Target>
+											<Button
+												p="xs"
+												variant="outline"
+												color="white"
+												onClick={(e) => {
+													e.stopPropagation();
+												}}
+												className="flex items-center justify-center"
+												style={{
+													border:
+														"calc(0.0625rem * var(--mantine-scale)) solid var(--paper-border-color)",
+												}}
+											>
+												<EllipsisVertical size={16} />
+											</Button>
+										</Menu.Target>
+										<Menu.Dropdown>
+											<Menu.Item
+												c="red"
+												onClick={(e) => {
+													e.stopPropagation();
+													deleteWorkout(workout.id);
+												}}
+											>
+												Delete
+											</Menu.Item>
+										</Menu.Dropdown>
+									</Menu>
+								</Group>
+
+								<Stack
+									mt="lg"
+									gap={8}
+								>
+									<Title
+										order={4}
+										mb="xs"
+									>
+										{workout.name}
+									</Title>
+									{workout.notes && <Text size="sm">{workout.notes}</Text>}
+
+									{workout.exercises.slice(0, 3).map((exercise, index) => (
+										<Group
+											gap="xs"
+											key={index}
+										>
+											<div
+												className="flex items-center justify-center w-6 h-6 rounded-md"
+												style={{
+													border:
+														"calc(0.0625rem * var(--mantine-scale)) solid var(--paper-border-color)",
+												}}
+											>
+												<Text size="sm">{index + 1}</Text>
+											</div>
+											<Text size="sm">{exercise.name}</Text>
+										</Group>
+									))}
+								</Stack>
+							</Card>
+						);
+					})}
+				</SimpleGrid>
+			</Stack>
 		</Container>
 	);
 };
