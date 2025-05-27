561a6c0802d4f75457f83d8cff17f6291bdd5f7b
Ray Orunsolu <rorunsolu@gmail.com>
2025-05-27 22:57:40 +0100

    renamed files

---
+++ src/pages/Routine/RoutineNew.tsx
@@ -0,0 +1,331 @@
+import { equipment, exerciseData, primaryMuscleGroups } from "@/assets/index";
+import { useExercisesHook } from "@/hooks/useExercisesHook";
+import { useRoutinesHook } from "@/hooks/useRoutinesHook";
+import { useDisclosure } from "@mantine/hooks";
+import { CheckCircle, Plus, Search } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+	Container,
+	Group,
+	Stack,
+	Text,
+	Button,
+	Table,
+	Modal,
+	TextInput,
+	Card,
+	Divider,
+	Select,
+	Input,
+} from "@mantine/core";
+const Routine = () => {
+	const [opened, { open, close }] = useDisclosure(false);
+	const [search, setSearch] = useState("");
+	const [selectedMuscle, setSelectedMuscle] = useState<string | null>(null);
+	const [selectedEquipment, setSelectedEquipment] = useState<string | null>(
+		null
+	);
+
+	const navigate = useNavigate();
+	const [name, setName] = useState("");
+	const [exercises, setExercises] = useState<
+		{
+			id: string;
+			name: string;
+			sets: { id: string; reps: string; weight: string }[];
+		}[]
+	>([]);
+
+	const { fetchExercises } = useExercisesHook();
+	const { createRoutine } = useRoutinesHook();
+
+	// Part of the logic for the add exercise modal
+	const filtered = exerciseData.filter((exercise) => {
+		const matchesSearch = exercise.name
+			.toLowerCase()
+			.includes(search.toLowerCase());
+		const matchesMuscle = selectedMuscle
+			? exercise.muscleGroup === selectedMuscle
+			: true;
+		const matchesEquipment = selectedEquipment
+			? exercise.equipment === selectedEquipment
+			: true;
+
+		const showAllMuscles = selectedMuscle === "All Muscles";
+		const showAllEquipment = selectedEquipment === "All Equipment";
+
+		return (
+			(matchesSearch && matchesMuscle && matchesEquipment) ||
+			showAllMuscles ||
+			showAllEquipment
+		);
+	});
+
+	const handleExerciseRender = (exercise: { name: string }) => {
+		setExercises((prev) => [
+			...prev,
+			{
+				id: Date.now().toString(),
+				name: exercise.name, // the type defintion (exercise: { name: string }) for the exercise paramater is for this ONLY
+				sets: [
+					// This is an ARRAY not just an object
+					{
+						id: Date.now().toString(), // the prev error was becasue i forgot to add the id property here
+						reps: "",
+						weight: "",
+					},
+				],
+			},
+		]);
+	};
+
+	const handleRowRender = (exerciseId: string) => {
+		setExercises((prev) =>
+			prev.map((exercise) =>
+				exercise.id === exerciseId
+					? {
+							...exercise,
+							sets: [
+								...exercise.sets,
+								{ id: Date.now().toString(), reps: "", weight: "" },
+							],
+						}
+					: exercise
+			)
+		);
+	};
+
+	const handleInputChange = (
+		exerciseId: string,
+		setId: string,
+		field: "reps" | "weight",
+		value: string
+	) => {
+		setExercises((prev) =>
+			prev.map((exercise) =>
+				exercise.id === exerciseId
+					? {
+							...exercise,
+							sets: exercise.sets.map((set) =>
+								set.id === setId ? { ...set, [field]: value } : set
+							),
+						}
+					: exercise
+			)
+		);
+	};
+
+	const handleRoutineUpload = async () => {
+		createRoutine(name, exercises);
+		setExercises([]);
+		setName("");
+	};
+
+	useEffect(() => {
+		fetchExercises();
+	}, []);
+
+	return (
+		<>
+			<Container
+				size="sm"
+				p="xs"
+				py="md"
+			>
+				<Stack gap="md">
+					<Stack gap="xs">
+						<TextInput
+							size="lg"
+							variant="unstyled"
+							value={name}
+							placeholder="Enter routine name"
+							onChange={(e) => setName(e.target.value)}
+						/>
+					</Stack>
+
+					<Stack gap="xl">
+						{exercises.map((exercise) => {
+							return (
+								<div key={exercise.id}>
+									<Group mb="xs">
+										<Text
+											fw={500}
+											size="lg"
+										>
+											{exercise.name}
+										</Text>
+									</Group>
+
+									<Table
+										striped
+										withRowBorders={false}
+									>
+										<Table.Thead>
+											<Table.Tr>
+												<Table.Th>Set</Table.Th>
+												<Table.Th>Weight</Table.Th>
+												<Table.Th>Reps</Table.Th>
+											</Table.Tr>
+										</Table.Thead>
+										<Table.Tbody>
+											{/* Accessing the exercises from the instanceOfexercises STATE and then rendering rows for each set */}
+											{/* The set in this case is coming from the array of sets INSIDE the instanceOfexercises STATE */}
+											{exercise.sets.map((set, index) => (
+												<Table.Tr key={index}>
+													<Table.Td>
+														<Text size="xs">{index + 1}</Text>
+													</Table.Td>
+													<Table.Td>
+														<TextInput
+															variant="unstyled"
+															placeholder="0kg"
+															value={set.weight}
+															onChange={(event) =>
+																handleInputChange(
+																	exercise.id, // this is refered to as exerciseId and setId as parameters inside the function
+																	set.id,
+																	"weight",
+																	event.currentTarget.value
+																)
+															}
+														/>
+													</Table.Td>
+													<Table.Td>
+														<TextInput
+															variant="unstyled"
+															placeholder="0"
+															value={set.reps}
+															onChange={(event) => {
+																handleInputChange(
+																	exercise.id,
+																	set.id,
+																	"reps",
+																	event.currentTarget.value
+																);
+															}}
+														/>
+													</Table.Td>
+												</Table.Tr>
+											))}
+										</Table.Tbody>
+									</Table>
+
+									<Group mt="md">
+										<Button
+											variant="light"
+											color="teal"
+											leftSection={<Plus size={20} />}
+											// pass the exercise.id taken from the id property of an exercise
+											onClick={() => handleRowRender(exercise.id)}
+										>
+											Add Set
+										</Button>
+									</Group>
+								</div>
+							);
+						})}
+					</Stack>
+
+					<Group
+						justify="center"
+						mt="md"
+					>
+						<Button
+							leftSection={<Plus size={20} />}
+							variant="default"
+							onClick={open}
+						>
+							Add Exercise
+						</Button>
+						<Button
+							leftSection={<CheckCircle size={20} />}
+							color="green"
+							onClick={() => {
+								handleRoutineUpload();
+								navigate("/home");
+							}}
+						>
+							Finish
+						</Button>
+					</Group>
+				</Stack>
+			</Container>
+
+			<Modal
+				opened={opened}
+				onClose={close}
+				title="Add Exercise"
+				fullScreen
+				radius={0}
+				transitionProps={{ transition: "fade", duration: 200 }}
+			>
+				<Stack gap="sm">
+					<Input
+						leftSection={<Search size={16} />}
+						placeholder="Search exercise"
+						value={search}
+						onChange={(e) => setSearch(e.target.value)}
+					/>
+
+					<Group grow>
+						<Select
+							defaultValue="All Equipment"
+							data={equipment}
+							clearable
+							searchable
+							nothingFoundMessage="Nothing found..."
+							checkIconPosition="right"
+							comboboxProps={{
+								transitionProps: { transition: "fade-down", duration: 200 },
+							}}
+							onChange={setSelectedEquipment}
+						/>
+						<Select
+							defaultValue="All Muscles"
+							data={primaryMuscleGroups}
+							clearable
+							searchable
+							nothingFoundMessage="Nothing found..."
+							checkIconPosition="right"
+							comboboxProps={{
+								transitionProps: { transition: "fade-down", duration: 200 },
+							}}
+							onChange={setSelectedMuscle}
+						/>
+					</Group>
+
+					<Divider />
+
+					<Stack>
+						{filtered.map((exercise, index) => (
+							<Card
+								key={index}
+								withBorder
+								radius="md"
+								p="sm"
+								style={{ cursor: "pointer" }}
+								onClick={() => {
+									handleExerciseRender(exercise);
+									close();
+								}}
+							>
+								<Group>
+									<Text fw={500}>{exercise.name}</Text>
+									<Text
+										size="xs"
+										c="dimmed"
+									>
+										{exercise.muscleGroup}
+									</Text>
+								</Group>
+							</Card>
+						))}
+					</Stack>
+				</Stack>
+			</Modal>
+		</>
+	);
+};
+
+export default Routine;
---
+++ src/pages/Routine/RoutinePage.tsx
@@ -0,0 +1,206 @@
+import { useRoutinesHook } from "@/hooks/useRoutinesHook";
+import { formatDistanceToNow } from "date-fns";
+import { EllipsisVertical, Play, Plus, Search, Trash } from "lucide-react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+	Button,
+	Container,
+	Group,
+	Stack,
+	Text,
+	Title,
+	SimpleGrid,
+	Card,
+	Menu,
+} from "@mantine/core";
+
+const RoutinePage = () => {
+	const navigate = useNavigate();
+	const { routines, fetchRoutines, deleteRoutine } = useRoutinesHook();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			await fetchRoutines();
+		};
+
+		fetchData();
+	}, []);
+
+	return (
+		<Container
+			size="md"
+			p="xs"
+			py="md"
+		>
+			<Stack gap="xl">
+				<Stack gap="xl">
+					<Stack gap={0}>
+						<Title order={1}>Routines</Title>
+						<Text c="dimmed">View all your routines and create new ones.</Text>
+					</Stack>
+
+					<SimpleGrid
+						spacing="md"
+						cols={{ base: 1, xs: 2, sm: 2, lg: 2 }}
+					>
+						<Button
+							color="teal"
+							variant="filled"
+							leftSection={<Plus size={20} />}
+							onClick={() => {
+								navigate("/routines");
+							}}
+							style={{ boxShadow: "0 1px 2px rgba(0, 0, 0, 0.1)" }}
+						>
+							New Routine
+						</Button>
+						<Button
+							color="teal"
+							variant="light"
+							leftSection={<Search size={20} />}
+							onClick={() => {
+								navigate("/explore");
+							}}
+							style={{ boxShadow: "0 1px 2px rgba(0, 0, 0, 0.1)" }}
+						>
+							Explore
+						</Button>
+					</SimpleGrid>
+				</Stack>
+
+				<Stack>
+					<Group
+						mb="xs"
+						mt="xs"
+					>
+						<Title order={3}>My Routines</Title>
+						<Card
+							radius="md"
+							shadow="md"
+							bg="dark.9"
+							withBorder
+							style={{
+								display: "flex",
+								alignItems: "center",
+								justifyContent: "center",
+								width: "30px",
+								height: "30px",
+							}}
+						>
+							{routines.length}
+						</Card>
+					</Group>
+
+					<SimpleGrid
+						spacing="md"
+						cols={{ base: 1, xs: 2, sm: 2, lg: 3 }}
+					>
+						{routines.map((routine, index) => (
+							<Card
+								key={index}
+								shadow="md"
+								padding="sm"
+								radius="md"
+								bg="dark.9"
+								withBorder
+							>
+								<Stack
+									justify="space-between"
+									style={{ flex: 1 }}
+								>
+									<Group
+										justify="space-between"
+										align="top"
+									>
+										<Stack gap={8}>
+											<Title order={4}>{routine.name}</Title>
+											<Text
+												size="sm"
+												mb="sm"
+											>
+												{routine.createdAt
+													? formatDistanceToNow(routine.createdAt.toDate(), {
+															addSuffix: true,
+														})
+													: "Unknown Date"}
+											</Text>
+
+											{routine.exercises.slice(0, 3).map((exercise, index) => (
+												<Group
+													gap="xs"
+													key={index}
+												>
+													<div
+														className="flex items-center justify-center w-6 h-6 rounded-md"
+														style={{
+															border:
+																"calc(0.0625rem * var(--mantine-scale)) solid var(--paper-border-color)",
+														}}
+													>
+														<Text size="sm">{index + 1}</Text>
+													</div>
+													<Text size="sm">{exercise.name}</Text>
+												</Group>
+											))}
+										</Stack>
+
+										<Menu
+											withinPortal
+											position="bottom-end"
+											shadow="md"
+										>
+											<Menu.Target>
+												<Button
+													p="xs"
+													variant="outline"
+													color="white"
+													onClick={(e) => {
+														e.stopPropagation();
+													}}
+													className="flex items-center justify-center"
+													style={{
+														border:
+															"calc(0.0625rem * var(--mantine-scale)) solid var(--paper-border-color)",
+													}}
+												>
+													<EllipsisVertical size={16} />
+												</Button>
+											</Menu.Target>
+											<Menu.Dropdown>
+												<Menu.Item
+													color="red"
+													leftSection={<Trash size={14} />}
+													onClick={(e) => {
+														e.stopPropagation();
+														deleteRoutine(routine.id);
+													}}
+												>
+													Delete
+												</Menu.Item>
+											</Menu.Dropdown>
+										</Menu>
+									</Group>
+									<Button
+										fullWidth
+										variant="filled"
+										color="teal"
+										leftSection={<Play size={20} />}
+										aria-label="Start routine"
+										onClick={() => {
+											navigate(`/routines/${routine.id}`);
+										}}
+									>
+										Start Routine
+									</Button>
+								</Stack>
+							</Card>
+						))}
+					</SimpleGrid>
+				</Stack>
+			</Stack>
+		</Container>
+	);
+};
+
+export default RoutinePage;
