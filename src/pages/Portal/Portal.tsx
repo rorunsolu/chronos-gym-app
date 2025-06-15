da99dbe86673a50b389e15f0f92d196a8cc95f1e
Ray Orunsolu <rorunsolu@gmail.com>
2025-06-15 16:02:40 +0100

    created unified sign up/in screens

--- src/pages/Portal/Portal.tsx
+++ src/pages/Portal/Portal.tsx
@@ -1,63 +1,75 @@
 import { UserAuth } from "@/auth/AuthContext";
 import ChronosLogo from "@/components/Branding/ChronosLogo";
 import GoogleLogo from "@/components/Branding/GoogleLogo";
-import { Anchor, Button, Container, Stack, Text } from "@mantine/core";
+import styles from "@/pages/Portal/tabs.module.css";
+import { isEmail, useForm } from "@mantine/form";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-//import { useAccountsHook } from "@/hooks/useAccountsHook";
+import {
+	Anchor,
+	Button,
+	Center,
+	Checkbox,
+	Container,
+	Divider,
+	Group,
+	PasswordInput,
+	Stack,
+	Tabs,
+	Text,
+	TextInput,
+	FloatingIndicator,
+} from "@mantine/core";
 
 const Portal = () => {
 	const navigate = useNavigate();
-	const [error, setError] = useState("");
-	const { googleSignIn, user } = UserAuth();
+	const { googleSignIn, emailSignUp, emailSignIn, user } = UserAuth();
 	const icon = <GoogleLogo />;
-	//const { isUserRegistered } = useAccountsHook();
 
 	const handleGoogleSignUp = async () => {
 		try {
 			await googleSignIn();
 		} catch (error) {
-			setError("Failed to create an account");
 			throw new Error("Failed to create an account");
 		}
 	};
 
-	const handleGoogleSignIn = async () => {
+	const form = useForm({
+		mode: "controlled",
+		initialValues: {
+			email: "",
+			password: "",
+		},
+		validate: {
+			email: isEmail("Invalid email"),
+			password: (value) => {
+				if (value.length < 6) {
+					return "Password must be at least 6 characters";
+				}
+
+				if (!/^(?=.*[A-Z])(?=.*\d).{1,}$/.test(value)) {
+					return "Password must have at least 1 capital letter and 1 number";
+				}
+				return null;
+			},
+		},
+	});
+
+	const handleEmailSignUp = async (email: string, password: string) => {
 		try {
-			await googleSignIn();
+			await emailSignUp(email, password);
 		} catch (error) {
-			setError("Failed to sign in");
-			throw new Error("Failed to sign in");
+			throw new Error("Failed to sign up. Please check your credentials.");
 		}
 	};
 
-	// useEffect(() => {
-	// 	if (user != null && isUserRegistered(user.uid)) {
-	// 		navigate("/home-page");
-	// 	}
-	// }, [user, isUserRegistered, navigate]);
-
-	// useEffect(() => {
-	// 	if (user != null && !isUserRegistered(user.uid)) {
-	// 		navigate("/register-form");
-	// 	}
-	// }, [user, isUserRegistered, navigate]);
-
-	// useEffect(() => {
-	// 	const checkRegistration = async () => {
-	// 		if (user != null) {
-	// 			const registered = await isUserRegistered(user.uid);
-
-	// 			if (registered) {
-	// 				navigate("/home-page");
-	// 			} else {
-	// 				navigate("/register-form");
-	// 			}
-	// 		}
-	// 	};
-
-	// 	checkRegistration();
-	// }, [user, isUserRegistered, navigate]);
+	const handleEmailSignIn = async (email: string, password: string) => {
+		try {
+			await emailSignIn(email, password);
+		} catch (error) {
+			throw new Error("Failed to log in. Please check your credentials.");
+		}
+	};
 
 	useEffect(() => {
 		if (user != null) {
@@ -65,55 +77,238 @@ const Portal = () => {
 		}
 	}, [user, navigate]);
 
+	const [activeTab, setActiveTab] = useState<"signup" | "login">("signup");
+	const [rootRef, setRootRef] = useState<HTMLDivElement | null>(null);
+	const [controlsRefs, setControlsRefs] = useState<
+		Record<string, HTMLButtonElement | null>
+	>({});
+	const setControlRef = (value: string) => (node: HTMLButtonElement) => {
+		controlsRefs[value] = node;
+		setControlsRefs(controlsRefs);
+	};
+
 	return (
 		<Container
-			size="xs"
+			size="sm"
 			p="md"
-			py="md"
 		>
-			<Stack
-				gap="xl"
-				align="center"
+			<Center
+				style={
+					{
+						//minHeight: "100vh",
+					}
+				}
 			>
-				<Stack align="center">
-					<ChronosLogo />
-					<h2>Welcome to the Chronos Gym app</h2>
-				</Stack>
-
-				<Stack align="center">
-					<Button
-						variant="default"
-						onClick={handleGoogleSignUp}
-						leftSection={icon}
-						fullWidth
+				<Stack
+					p="lg"
+					style={{
+						maxWidth: 400,
+						width: "100%",
+					}}
+				>
+					<Group
+						justify="center"
+						mb="md"
 					>
-						Register with Google
-					</Button>
+						<ChronosLogo />
+					</Group>
 
-					<Text
-						size="sm"
-						c="dimmed"
+					<Tabs
+						color="teal"
+						variant="none"
+						value={activeTab}
+						onChange={(value) => setActiveTab(value as "signup" | "login")}
+						mb="lg"
 					>
-						Already have an account?{" "}
-						<Anchor
-							component={Link}
-							to=""
-							onClick={handleGoogleSignIn}
+						<Tabs.List
+							grow
+							ref={setRootRef}
+							className={styles.list}
 						>
-							Sign in with Google
-						</Anchor>
-					</Text>
-				</Stack>
+							<Tabs.Tab
+								value="signup"
+								ref={setControlRef("signup")}
+								className={styles.tab}
+								// style={{ borderRadius: "calc(0.25rem * 1)" }}
+							>
+								Sign up
+							</Tabs.Tab>
+							<Tabs.Tab
+								value="login"
+								ref={setControlRef("login")}
+								className={styles.tab}
+							>
+								Log in
+							</Tabs.Tab>
 
-				{error && (
-					<Text
-						c="red"
-						size="sm"
-					>
-						{error}
-					</Text>
-				)}
-			</Stack>
+							<FloatingIndicator
+								target={activeTab ? controlsRefs[activeTab] : null}
+								parent={rootRef}
+								className={styles.indicator}
+							/>
+						</Tabs.List>
+					</Tabs>
+
+					{activeTab === "signup" ? (
+						<>
+							<form
+								onSubmit={form.onSubmit((values) =>
+									handleEmailSignUp(values.email, values.password)
+								)}
+							>
+								<TextInput
+									{...form.getInputProps("email")}
+									label="Email"
+									placeholder="Enter your email"
+									mb="md"
+									withAsterisk
+								/>
+								<PasswordInput
+									{...form.getInputProps("password")}
+									label="Password"
+									placeholder="Create a password"
+									mb={4}
+									withAsterisk
+								/>
+								<Stack
+									gap="5"
+									mb="lg"
+									mt="md"
+								>
+									<Group
+										gap={8}
+										align="center"
+									>
+										<Checkbox
+											checked={form.values.password.length >= 8}
+											readOnly
+											size="xs"
+										/>
+										<Text
+											fz="xs"
+											c="dimmed"
+										>
+											Must be at least 8 characters
+										</Text>
+									</Group>
+									<Group
+										gap={8}
+										align="center"
+									>
+										<Checkbox
+											checked={/[^A-Za-z0-9]/.test(form.values.password)}
+											readOnly
+											size="xs"
+										/>
+										<Text
+											fz="xs"
+											c="dimmed"
+										>
+											Must contain one special character
+										</Text>
+									</Group>
+								</Stack>
+								<Button
+									type="submit"
+									fullWidth
+									color="teal"
+									mb="md"
+								>
+									Sign up
+								</Button>
+								<Divider
+									label="or"
+									labelPosition="center"
+									mb="md"
+								/>
+								<Button
+									variant="default"
+									leftSection={icon}
+									fullWidth
+									mb="md"
+									onClick={handleGoogleSignUp}
+								>
+									Sign up with Google
+								</Button>
+							</form>
+							<Text
+								ta="center"
+								fz="sm"
+								mt="md"
+							>
+								Already have an account?{" "}
+								<Anchor
+									component={Link}
+									to=""
+									onClick={() => setActiveTab("login")}
+								>
+									Log in
+								</Anchor>
+							</Text>
+						</>
+					) : (
+						<>
+							<form
+								onSubmit={form.onSubmit((values) =>
+									handleEmailSignIn(values.email, values.password)
+								)}
+							>
+								<TextInput
+									{...form.getInputProps("email")}
+									label="Email"
+									placeholder="Enter your email"
+									mb="md"
+									withAsterisk
+								/>
+								<PasswordInput
+									{...form.getInputProps("password")}
+									label="Password"
+									placeholder="Enter your password"
+									mb="md"
+									withAsterisk
+								/>
+								<Button
+									type="submit"
+									fullWidth
+									color="teal"
+									mb="md"
+								>
+									Log in
+								</Button>
+								<Divider
+									label="or"
+									labelPosition="center"
+									mb="md"
+								/>
+								<Button
+									variant="default"
+									leftSection={icon}
+									fullWidth
+									mb="md"
+									onClick={handleGoogleSignUp}
+								>
+									Log in with Google
+								</Button>
+							</form>
+
+							<Text
+								ta="center"
+								fz="sm"
+								mt="md"
+							>
+								Don''t have an account?{" "}
+								<Anchor
+									component={Link}
+									to=""
+									onClick={() => setActiveTab("signup")}
+								>
+									Sign up
+								</Anchor>
+							</Text>
+						</>
+					)}
+				</Stack>
+			</Center>
 		</Container>
 	);
 };
